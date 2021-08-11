f3caf2155873a293bf3186d707956f6896032469
qaz912187 <912187964@qq.com>
2021-08-11 21:40:56 +0800

    细节修改 a Please enter the commit message for your changes. Lines starting

--- settings.h
+++ settings.h
@@ -13,9 +13,11 @@ public:
     static bool discoverable();
     static quint16 serverPort();
     static quint16 DiscoveryPort();
+    static quint16 WebPort();
     static void setDeviceName(const QString &deviceName);
     static void setDownloadPath(const QString &downloadPath);
     static void setDiscoverable(bool discoverable);
     static void setServerPort(quint16 serverPort);
     static void setDiscoveryPort(quint16 discoveryPort);
+    static void setWebPort(quint16 webPort);
 };
